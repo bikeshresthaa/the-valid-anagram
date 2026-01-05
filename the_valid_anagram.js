f835540a8351b19711af7f5188359a31b9caf0ec
bikeshresthaa <eshbikesh@gmail.com>
2026-01-05 11:57:35 +0545

    Break single line comments to multilines

--- the_valid_anagram.js
+++ the_valid_anagram.js
@@ -1,8 +1,13 @@
 
-// the main idea is to record the frequency of each character in the first string and decrement       the frequency of occurance of each character in the second string. Both strings share the same frequency map so that when the decrementing process starts if any character's frequency goes below zero or the character is not found in the frequency map we can say that the two strings are not anagrams of each other.
+// the main idea is to record the frequency of each character in the first string and 
+// decrement the frequency of occurance of each character in the second string. Both 
+// strings share the same frequency map so that when the decrementing process starts 
+// if any character's frequency goes below zero or the character is not found in the 
+// frequency map we can say that the two strings are not anagrams of each other.
 
 const checkAnagram = (str1, str2) => {
-  // the first condition checks if the lengths of both the strings are equal. If not equal we can easily conclude that they are not anagrams of eachother.
+  // the first condition checks if the lengths of both the strings are equal. 
+  // If not equal we can easily conclude that they are not anagrams of eachother.
   if(str1.length !== str2.length){
     return false;
   }
